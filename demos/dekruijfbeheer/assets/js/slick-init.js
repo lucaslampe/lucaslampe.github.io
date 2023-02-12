4560fd73d17321905ee3320077f309805e93a3d2
Lucas Lampe <lucaslampe@gmail.com>
2023-02-12 12:21:02 +0100

    De Kruijf Beheer

---
+++ demos/dekruijfbeheer/assets/js/slick-init.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.slider-area').slick({
+      centerMode: true,
+      slidesToShow: 1,
+      autoplay: true,
+      variableWidth: true,
+      infinite: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      responsive: [
+          {
+          breakpoint: 768,
+          settings: {
+              arrows: false,
+              centerMode: true,
+              slidesToShow: 1
+          }
+          },
+          {
+          breakpoint: 480,
+          settings: {
+              arrows: false,
+              centerMode: true,
+              slidesToShow: 1
+          }
+          }
+      ]
+      });
+  });
